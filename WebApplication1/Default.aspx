dcf5777eef4380bfdb6501b74e4c17ccc6429a82
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2024-09-02 18:02:15 -0300

    Add project files.

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView runat="server" ID="dgvAddresses" CssClass="table table-dark table-bordered" AutoGenerateColumns="false">
+        <Columns>
+            <asp:BoundField HeaderText="Street" DataField="Street" />
+            <asp:BoundField HeaderText="Number" DataField="Number" />
+        </Columns>
+    </asp:GridView>
+
+</asp:Content>
---
+++ domain/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Street { get; set; }
+    }
+    public class AdressService
+    {
+        public List<Address> list()
+        {
+            List<Address> list = new List<Address>();
+            list.Add(new Address());
+            list.Add(new Address());
+
+            list[0].Id = 1;
+            list[0].Number = 143;
+            list[0].Street = "Calle 1";
+
+            list[1].Id = 2;
+            list[1].Number = 566;
+            list[1].Street = "Calle 65";
+
+            return list;
+        }
+    }
+}
